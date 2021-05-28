d12c6d96c0d905bd232df73ab2b63a61cb9b4ebe
gonzalokesy <gonzalo.kesy@gmail.com>
2021-05-28 16:39:03 -0300

    Primer subida del proyecto

---
+++ app.js
@@ -0,0 +1,28 @@
+// ------- Requiriendo express. 
+const express = require ('express');
+const app = express (); 
+
+
+// ------- Requiriendo path y estableciendo carpeta estática.  
+const path = require ('path'); 
+const publicPath = path.resolve( __dirname, './public');
+
+app.use(express.static(publicPath))
+
+// ------- Levantando servidor. 
+app.listen (3030, () => {
+    console.log('Servidor corriendo corriendo correctamente en http://localhost:3030/');
+})  
+
+// ------- Levantando servidor.
+app.get('/', (req, res) => { 
+    res.sendFile (path.resolve( __dirname, './views/home.html'))
+})
+
+app.get('/register', (req, res) => { 
+    res.sendFile (path.resolve( __dirname, './views/register.html'))
+})
+
+app.get('/login', (req, res) => { 
+    res.sendFile (path.resolve( __dirname, './views/login.html'))
+})
